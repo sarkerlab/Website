578158c1acfa3a59904cff6e7de6155e49ce9bd0
Mohammedali87 <57373599+Mohammedali87@users.noreply.github.com>
2019-11-25 14:15:11 -0500

    Create 2017-01-01-Overview of the Second Social Media Mining for Health (SMM4H) shared tasks at AMIA 2017.m

---
+++ _posts/2017-01-01-Overview of the Second Social Media Mining for Health (SMM4H) shared tasks at AMIA 2017.m
@@ -0,0 +1,13 @@
+---
+authors: "Abeed Sarker, Graciela Gonzalez-Hernandez"
+title: "Overview of the Second Social Media Mining for Health (SMM4H) shared tasks at AMIA 2017"
+additional_info: "Training"
+external_url: http://ceur-ws.org/Vol-1996/paper8.pdf
+categories:
+  - Social Media
+data:
+code:
+resource:
+---
+
+
